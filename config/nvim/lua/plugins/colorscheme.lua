3cf2eaae433ddb6b105b029c8a4e24eb6d6ddb45
Yuri <ya.augusto@gmail.com>
2024-06-27 00:15:55 -0300

    Change colorschemes

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,19 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+			enabled = false,
+		},
+	},
+
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		config = function()
+			require("onedark").setup({ style = "darker" })
+			require("onedark").load()
+		end,
+	},
 }
